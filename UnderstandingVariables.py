21aaa1d8eac96cde50254b0899c9ffa05db5308c
singhsupriya2819 <singhsupriya2819@gmail.com>
2026-07-05 16:18:40 +0530

    Commit variables

---
+++ UnderstandingVariables.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x = 5
+y = "Supriya"
+print(x)
+print(y)
+
+code = "code"
+print(code)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to coding")
